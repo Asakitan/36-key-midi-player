438091dbfd5baa4e25f6fa67c36cc229410502c9
Asakitan <Asakitan@users.noreply.github.com>
2026-03-24 13:37:37 +1100

    chore: bump version v3.3.6+3306  v3.3.7+3307

--- sao_gui.py
+++ sao_gui.py
@@ -3258,7 +3258,7 @@ class SAOPlayerGUI:
             self._sao_menu.close()
         self.root.after(600, lambda: SAODialog.showinfo(
             self._float, "关于",
-            "咲 Midi Player  SAO Edition\nv3.3.6+3306\n\n"
+            "咲 Midi Player  SAO Edition\nv3.3.7+3307\n\n"
             "Alt+A 打开 SAO 菜单\n"
             "右键悬浮按钮查看更多选项"))
 
